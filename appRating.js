7a3603e91965caff68d619eff3c1cc965f23fd1d
Chris Young <youngcm2@gmail.com>
2011-06-20 14:58:39 -0500

    updating scope

--- appRating.js
+++ appRating.js
@@ -1,19 +1,22 @@
 /**
  * @author Chris Young
  */
-var appRating =  {};
+var appRating = {};
 
-(function(){
+(function() {
 	appRating.daysUntilPrompt= 5;
 	appRating.timeBeforeReminder= 1;
 	appRating.usesUntilPrompt= 3;
-	appRating.ratingMessage= String.format('If you enjoy using %@, would you mind taking a moment to rate it? It won\'t take more than a minute. Thanks for your support!', Ti.App.getName());
-	appRating.messageTitle= String.format('Rate %@', Ti.App.getName());
-	appRating.cancelButton= 'No, Thanks';
-	appRating.rateButton= String.format('Rate %@', Ti.App.getName());
-	appRating.remindMeLater= 'Remind Me later';
-	appRating.templateReviewURL= 'itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@';
 	appRating.appId = 'Your App ID';
+
+	/*Private */
+	var messageTitle= String.format('Rate %@', Ti.App.getName());
+	var templateReviewURL= 'itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=%@';
+	var rateButton= String.format('Rate %@', Ti.App.getName());
+	var ratingMessage= String.format('If you enjoy using %@, would you mind taking a moment to rate it? It won\'t take more than a minute. Thanks for your support!', Ti.App.getName());
+	var cancelButton= 'No, Thanks';
+	var remindMeLater= 'Remind Me later';
+
 	incrementUseCount = function incrementUseCount () {
 		var version = Ti.App.getVersion();
 
@@ -47,11 +50,10 @@ var appRating =  {};
 			Ti.App.Properties.setDouble('ReminderRequestDate', 0);
 		}
 	}
-	
 	appRating.ratingConditionsHaveBeenMet = function ratingConditionsHaveBeenMet() {
 		var dateOfFirstLaunch = Ti.App.Properties.getDouble('FirstUseDate');
 		var timeSinceFirstLaunch = new Date().getTime() - dateOfFirstLaunch;
-		var timeUntilRate = 60 * 60 * 24 * this.daysUntilPrompt;
+		var timeUntilRate = 60 * 60 * 24 * appRating.daysUntilPrompt;
 		Ti.API.info('First Launch: ' + dateOfFirstLaunch);
 		Ti.API.info('Since First Launch: ' + timeSinceFirstLaunch);
 		Ti.API.info('Time Until Rate: ' + timeUntilRate);
@@ -62,7 +64,7 @@ var appRating =  {};
 
 		var useCount = Ti.App.Properties.getInt('UseCount');
 		Ti.API.info('Used: ' + useCount);
-		if(useCount <= this.usesUntilCount) {
+		if(useCount <= appRating.usesUntilPrompt) {
 			return false;
 		}
 
@@ -76,7 +78,7 @@ var appRating =  {};
 
 		var reminderRequestDate = Ti.App.Properties.getDouble('ReminderRequestDate');
 		var timeSinceReminderRequest = new Date().getTime() - reminderRequestDate;
-		var timeUntilReminder = 60 * 60 * 24 * this.timeBeforeReminder;
+		var timeUntilReminder = 60 * 60 * 24 * appRating.timeBeforeReminder;
 
 		if(timeSinceReminderRequest < timeUntilReminder) {
 			return false;
@@ -84,19 +86,17 @@ var appRating =  {};
 
 		return true;
 	}
-	
 	appRating.incrementAndRate= function incrementAndRate() {
-		this.incrementUseCount();
-		if(this.ratingConditionsHaveBeenMet()) {
-			this.showRatingAlert();
+		incrementUseCount();
+		if(appRating.ratingConditionsHaveBeenMet()) {
+			appRating.showRatingAlert();
 		}
 	}
-	
 	appRating.showRatingAlert = function showRatingAlert() {
 		var a = Titanium.UI.createAlertDialog({
-			title: this.messageTitle,
-			message: this.ratingMessage,
-			buttonNames: [this.rateButton, this.remindMeLater, this.cancelButton],
+			title: messageTitle,
+			message: ratingMessage,
+			buttonNames: [rateButton, remindMeLater, cancelButton],
 			cancelButton: 2
 		});
 
@@ -106,8 +106,8 @@ var appRating =  {};
 					Ti.API.info('Rate ' + Ti.Platform.model);
 					if(Ti.Platform.model == 'simulator') {
 						Ti.API.info('Launching rating url');
-					} else {						
-						var reviewUrl = String.format(apprating.templateReviewURL, apprating.appId);
+					} else {
+						var reviewUrl = String.format(templateReviewURL, appRating.appId);
 						Ti.App.Properties.setBool('RatedCurrentVersion', true);
 						Ti.API.info('Launching rating url: ' + reviewUrl);
 						Ti.Platform.openURL(reviewUrl);
@@ -128,37 +128,29 @@ var appRating =  {};
 		a.show();
 	}
 })();
-
-(function() {	
+(function() {
 	appRating.incrementAndRate();
-if (isiOS4Plus())
-{
-	// fired when an app resumes for suspension
-	Ti.App.addEventListener('resume',function(e){
-		apprating.incrementAndRate();
-	});
-	Ti.App.addEventListener('resumed',function(e){
-		apprating.incrementAndRate();
-	});
-}
+	if (isiOS4Plus()) {
+		// fired when an app resumes for suspension
+		Ti.App.addEventListener('resume', function(e) {
+			appRating.incrementAndRate();
+		});
+		Ti.App.addEventListener('resumed', function(e) {
+			appRating.incrementAndRate();
+		});
+	}
 
 })();
-
-
-function isiOS4Plus()
-{
+function isiOS4Plus() {
 	// add iphone specific tests
-	if (Titanium.Platform.name == 'iPhone OS')
-	{
+	if (Titanium.Platform.name == 'iPhone OS') {
 		var version = Titanium.Platform.version.split(".");
 		var major = parseInt(version[0]);
-		
+
 		// can only test this support on a 3.2+ device
-		if (major >= 4)
-		{
+		if (major >= 4) {
 			return true;
 		}
 	}
 	return false;
 }
-
